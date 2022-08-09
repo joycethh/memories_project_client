6266d0ddde05d055b6e9e1d2d2e039104e5d9ab5
joycethh <joycethh@outlook.com>
2022-08-09 16:47:08 -0500

    layout the basic structure for the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import React from "react";
-
+import { Container, AppBar, Typography, Grid, Grow } from "@material-ui/core";
+import memories from "./assets/image/memories.png";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
 const App = () => {
-  return <h1> Memories App</h1>;
+  return (
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="strech"
+            spacing={2}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
 };
 
 export default App;
---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Post from "./post/Post";
+const Posts = () => {
+  return (
+    <>
+      <Post />
+      <div>Posts</div>;
+      <Post />
+    </>
+  );
+};
+
+export default Posts;
