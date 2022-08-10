13ce7c0dc82e904f02e38994266e207f36704aba
joycethh <joycethh@outlook.com>
2022-08-10 16:47:13 -0500

    POST a single post works. But there is probelm with fetching all the data

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Grid, CircularProgress } from "@material-ui/core";
 import { useSelector } from "react-redux";
 
 import Post from "./post/Post";
@@ -7,14 +8,22 @@ import useStyles from "./styles";
 const Posts = () => {
   const classes = useStyles();
   const posts = useSelector((state) => state.posts);
+
   //the "posts" is from combinereduder in reducers file
   console.log(posts);
   return (
-    <>
-      <div>Posts</div>
-      <Post />
-      <Post />
-    </>
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
--- src/components/Posts/post/Post.js
+++ src/components/Posts/post/Post.js
@@ -1,8 +1,68 @@
 import React from "react";
+import {
+  Card,
+  Button,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@material-ui/core";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import DeleteIcon from "@material-ui/icons/Delete";
+
+import moment from "moment";
+
 import useStyles from "./styles";
-const Post = () => {
+
+const Post = ({ post }) => {
   const classes = useStyles();
-  return <div>Post</div>;
+  return (
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.media}
+        image={post.selectedFile}
+        title={post.title}
+      />
+      <div className={classes.overlay}>
+        <Typography variant="h6">{post.creator}</Typography>
+        <Typography variant="body2">
+          {moment(post.createdAt).fromNow()}
+        </Typography>
+      </div>
+      <div className={classes.overlay2}>
+        <Button style={{ color: "white" }} size="small" onClick={() => {}}>
+          <MoreHorizIcon fontSize="medium" />
+        </Button>
+      </div>
+      <div className={classes.details}>
+        <Typography variant="body2" color="textSecondary">
+          {post.tags.map((tag) => `#${tag} `)}
+        </Typography>
+      </div>
+      <Typography
+        className={classes.title}
+        variant="h5"
+        compoennt="h2"
+        gutterBottom
+      >
+        {post.title}
+      </Typography>
+      <CardContent>
+        <Typography className={classes.title} variant="body2" component="p">
+          {post.title}
+        </Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="primary" onClick={() => {}}>
+          <ThumbUpAltIcon /> Like {post.likeCount}
+        </Button>
+        <Button size="small" color="secondary" onClick={() => {}}>
+          <DeleteIcon />
+        </Button>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default Post;
