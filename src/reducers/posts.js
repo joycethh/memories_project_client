23bcbc1326374ba5b3c5fa177f671767f862d21a
joycethh <joycethh@outlook.com>
2022-08-11 16:47:09 -0500

    Define PATCH. 1. Reducer, define the case, return single post or the state value if there is any updates

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -3,15 +3,16 @@ export default (posts = [], action) => {
   console.log("action", action);
   switch (action.type) {
     case "FETCH_ALL": {
-      console.log("fetch_all", posts);
       return action.payload;
     }
-    //The logic here is that we renanme the "state" as posts,
-    //and since we dipatch the function in action folder, we got the data back through action.payload
-    //we can return action.payload,instead of "state";
     case "CREATE": {
-      console.log("create", posts);
       return [...posts, action.payload];
+    }
+    case "UPDATE": {
+      // return [...posts, action.payload];
+      return posts.map((post) =>
+        post._id === action.payload._id ? action.payload : post
+      );
     }
     default:
       return posts;
