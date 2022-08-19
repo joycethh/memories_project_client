fe3b00df09dd7574ae77c611057491f7ef845522
joycethh <joycethh@outlook.com>
2022-08-19 11:07:21 -0500

    add more routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import "dotenv/config";
 import React from "react";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Container } from "@material-ui/core";
+
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
+import PostDetails from "./components/PostDetails/PostDetails";
 import Auth from "./components/Auth/Auth";
 
 const App = () => {
   return (
     <GoogleOAuthProvider clientId={`${process.env.REACT_APP_GOOGLE_ID}`}>
       <BrowserRouter>
-        <Container maxWidth="lg">
+        <Container maxWidth="xl">
           <Navbar />
           <Routes>
-            <Route path="/" exact element={<Home />} />
-            <Route path="/auth" exact element={<Auth />} />
+            {/* <Route path="/" exact element={<Home />} /> */}
+            <Route path="/" element={<Navigate replace to="/posts" />} />
+            <Route path="/posts" element={<Home />} />
+            <Route path="/posts/search" element={<Home />} />
+            <Route path="/posts/:id" element={<PostDetails />} />
+            <Route path="/auth" element={<Auth />} />
           </Routes>
         </Container>
       </BrowserRouter>
