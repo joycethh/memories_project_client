9d2a38ecd6e4ce3f64e3a71eacfb429cfef14d11
joycethh <joycethh@outlook.com>
2022-08-20 19:50:20 -0500

    adding isLoading status--1. create const

--- src/contants/actionType.js
+++ src/contants/actionType.js
@@ -8,3 +8,6 @@ export const AUTH = "AUTH";
 export const LOGOUT = "LOGOUT";
 
 export const FETCH_BYSEARCH = "FETCH_BYSEARCH";
+
+export const START_LOADING = "START_LOADING";
+export const END_LOADING = "END_LOADING";
