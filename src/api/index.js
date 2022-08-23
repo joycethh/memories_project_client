47d8996cb2d079f3dc0dd041e60f9765d9da34d5
joycethh <joycethh@outlook.com>
2022-08-23 13:24:06 -0500

    get ready for depolyment

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({
+  baseURL: "https://mern-memories-social-media-app.herokuapp.com/",
+});
 
 //send token on each request so that AUth middleware can work;
 //check the user exists, and then get token.
